0ea6b5243302eb3905fd6669a7b33632e989a505
John Leonard <john@lowkeylabs.com>
2024-11-18 15:29:47 -0500

    closer

--- mcb/src/App.tsx
+++ mcb/src/App.tsx
@@ -5,7 +5,6 @@ function App() {
   return (
     <div className="h-screen w-screen bg-gray-200 flex items-center justify-center">
       <div className="bg-white w-[90%] max-w-md aspect-[9/16] rounded-lg shadow-md">
-        <h1 className="text-center text-2xl font-bold">Hello, Tailwind with TypeScript!</h1>
         <MyChatBot />
       </div>
     </div>
--- mcb/src/MyChatBot.tsx
+++ mcb/src/MyChatBot.tsx
@@ -2,11 +2,23 @@ import ChatBot, { useChatWindow } from "react-chatbotify";
 
 const MyChatBot = () => {
     const settings = {
-        general: {embedded: true},
-        chatHistory: {storageKey: "playground"},
-        botBubble: {simStream: true},
+        general: {
+            embedded: true,
+            showHeader: false,
+            showFooter: false,
+            secondaryColor: "red",
+        },
+        //device: {desktopEnabled: true},
+        //chatHistory: {storageKey: "playground"},
+        //botBubble: {simStream: true},
+        //chatWindow: {defaultOpen:true},
     }
     const styles = {
+        chatWindowStyle: {
+            width:"100%",
+            height:"100%",
+            overflow: "hidden",
+        },
     }
 
     const flow={
