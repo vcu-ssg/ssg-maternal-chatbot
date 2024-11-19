b157ca78321a19b299a711a4594659c5cbf8503c
John Leonard <john@lowkeylabs.com>
2024-11-18 22:10:31 -0500

    working

--- mcb/src/App.tsx
+++ mcb/src/App.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import MyChatBot from "./MyChatBot";
 
+// <div className="h-screen w-screen bg-gray-200 flex items-center justify-center">
+//<div className="bg-white w-[90%] max-w-md aspect-[9/16] rounded-lg shadow-md">
+
 function App() {
   return (
-    <div className="h-screen w-screen bg-gray-200 flex items-center justify-center">
-      <div className="bg-white w-[90%] max-w-md aspect-[9/16] rounded-lg shadow-md">
+    <div>
+      <div>
         <MyChatBot />
       </div>
     </div>
